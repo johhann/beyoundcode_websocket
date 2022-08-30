d28ded3395607f2c20291f03ce842ea9dd37dbcc
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-08-30 14:03:02 +0300

    websocket configuration

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,19 +36,15 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('LARAVEL_WEBSOCKET_HOST'),
-                'PORT' => env('LARAVEL_WEBSOCKET_PORT'),
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST', 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'useTLS' => false,
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-                'curl_options' => [
+                "host" => env('LARAVEL_WEBSOCKET_HOST'),
+                "port" => env('LARAVEL_WEBSOCKET_PORT'),
+                "cluster" => env('PUSHER_APP_CLUSTER'),
+                "useTLS" => false,
+                "scheme" => 'http',
+                "curl_options" => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
-                ]
-                // 'encrypted' => true,
-                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                ],
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
