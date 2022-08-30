15fe56fff665d13c8c1cb113c8c9cd8606129569
Yohannes Mekonnen <johannesm1219@gmail.com>
2022-08-30 11:50:11 +0300

    broadcast configuration

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,11 +36,19 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
+                'cluster' => env('LARAVEL_WEBSOCKET_HOST'),
+                'PORT' => env('LARAVEL_WEBSOCKET_PORT'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST', 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
+                'useTLS' => false,
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
+                // 'encrypted' => true,
+                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
